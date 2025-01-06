e68291cd7bf7bbd457cbb87667d4e23f0d545c38
Sir.D3V4L07 <85847841+SirD3V4L07@users.noreply.github.com>
2025-01-06 16:52:38 +0000

    Make function non IIFE

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ console.log("Hello world 3");
 
 let playerSign = "N/A";
 
-(
-    function changeCells(playerSign) {
-        const cells = document.querySelectorAll(".cell");
-        cells.forEach((cell, index) => {
-            cell.addEventListener('click', () => {
-                cell.innerHTML = playerSign;
-            });
+
+function changeCells(playerSign) {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            cell.innerHTML = playerSign;
         });
-    }
-)();
+    });
+};
+
+changeCells(playerSign);
