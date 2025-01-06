d87b5f532624ab5561c2c15882467f0f6258187f
Sir.D3V4L07 <85847841+SirD3V4L07@users.noreply.github.com>
2025-01-06 16:32:32 +0000

    Correct syntax

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let playerSign = "N/A";
 (
     function changeCells() {
         const cells = document.querySelectorAll(".cell");
-        cells.forEach(cell, index) => {
+        cells.forEach((cell, index) => {
             cell.addEventListener('click', () => {
                 cell.innerHTML = "X";
-            })
-        };
-    };
+            });
+        });
+    }
 )();
