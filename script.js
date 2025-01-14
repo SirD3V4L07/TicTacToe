e0aea2a51ce20766462f4ce8cb6561c5379ca11d
Sir.D3V4L07 <85847841+SirD3V4L07@users.noreply.github.com>
2025-01-14 18:19:04 +0000

    Change changeCells function to modular The idea is to make it work in different scenarios like a black box.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log("09/01/2025 13:04");
 
 /**Variables */
+let activeSign = "N/A";
 let playerSign = "N/A";
+let comSign = "N/A"
 let board = [1,2,3,4,5,6,7,8,9];
 
 /**Functions */
-function changeCells(playerSign) {
+function changeCells(activeSign) {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell, index) => {
         cell.addEventListener('click', () => {
-            board[index] = playerSign;
+            board[index] = activeSign;
             cell.innerHTML = board[index];
             console.log(board[index]);
         });
@@ -17,5 +19,5 @@ function changeCells(playerSign) {
 };
 
 /**Main Body */
-changeCells(playerSign);
+changeCells(activeSign);
 
