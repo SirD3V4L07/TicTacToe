c703a5b44963809607765f8d74fb3e84f650fdb5
Sir.D3V4L07 <85847841+SirD3V4L07@users.noreply.github.com>
2025-01-14 18:44:54 +0000

    Add checkWin

--- script.js
+++ script.js
@@ -22,8 +22,27 @@ function resetBoard()  {
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 };
 
-function checkWin() {
+function checkWin(activeSign, board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] === activeSign && board[b] === activeSign && board[c] === activeSign) {            
+            console.log("Winner found");
+            return true;
+        }
+    }
+    console.log("No winner yet..");
+    return false;
 }
 
 /**Main Body */
